4b2ee74254a7d0263d583b4982051587c0df9958
sanyath007 <sanyath007@gmail.com>
2020-11-29 14:54:58 +0700

    Add gulp commands to gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-clean-css');
+var cachebust = require('gulp-cache-bust');
+var replace = require('gulp-string-replace');
+var del = require('del');
+const { minify } = require('uglify-js');
+var versionTimeStamp = "" + Date.now();
+
+gulp.task('delete_all', function() {
+    return del([
+        'public/*.*',
+        'public/css/*.*',
+        'public/img/*.*',
+        'public/js/*.*',
+        'public/templates/*.*'
+    ]);
+});
+
+gulp.task('minify', function() {
+    return gulp.src('index.html')
+            .pipe(useref())
+            .pipe(replace('___REPLACE_IN_GULP___', versionTimeStamp))
+            .pipe(gulpif('*.js', uglify()))
+            .pipe(gulpif('*.css', minifyCss()))
+            .pipe(cachebust({
+                type: 'timestamp'
+            }))
+            .pipe(gulp.dest('public'));
+});
+
+gulp.task('copy1', function() {
+    return gulp.src('templates/*.*')
+            .pipe(gulp.dest('public/templates'));
+});
+
+gulp.task('copy2', function() {
+    return gulp.src('img/*.*')
+            .pipe(gulp.dest('public/img'));
+});
+
+gulp.task('default', gulp.series('delete_all', 'minify', 'copy1', 'copy2'));
