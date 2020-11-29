68686772e30a07c8c515cfa8c9bb732dd5cb6772
sanyath007 <sanyath007@gmail.com>
2020-11-29 15:02:13 +0700

    Comment app.js on section to intercept request

--- js/app.js
+++ js/app.js
@@ -1,47 +1,52 @@
-	var app = angular.module('App', ['ngRoute']);
+var app = angular.module('App', ['ngRoute']);
 
-	app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function($routeProvider) {
 
-		$routeProvider
+	$routeProvider
 
-		.when('/', {
+	.when('/', {
 	    templateUrl: 'templates/home.html',
 	    controller: 'homeController'
-	  })
+	})
 
-	  .when('/page1', {
+	.when('/page1', {
 	    templateUrl: 'templates/page1.html',
 	    controller: 'page1Controller'
-	  })
+	})
 
-	  .when('/page2', {
+	.when('/page2', {
 	    templateUrl: 'templates/page2.html',
 	    controller: 'page2Controller'
-	  });
-	}]);
+	});
+}]);
 
-	app.controller('homeController', ['$scope', function($scope) {
-	  $scope.message = 'Message on home page';
-	}]);
+app.controller('homeController', ['$scope', function($scope) {
+	$scope.message = 'Message on home page';
+}]);
 
-	app.controller('page1Controller', ['$scope', function($scope) {
-	  $scope.message = 'Message on page 1';
-	}]);
+app.controller('page1Controller', ['$scope', function($scope) {
+	$scope.message = 'Message on page 1';
+}]);
 
-	app.controller('page2Controller', ['$scope', function($scope) {
-	  $scope.message = 'Message on page 2';
-	}]);
+app.controller('page2Controller', ['$scope', function($scope) {
+	$scope.message = 'Message on page 2';
+}]);
 
-	app.service('preventTemplateCache', [function() {
+/**
+ * ==================================================
+ *  Intercept every request to templates directory
+ * ==================================================
+ */
+app.service('preventTemplateCache', [function() {
     var service = this;
     service.request = function(config) {
-      if (config.url.indexOf('templates') !== -1) {
-        config.url = config.url + '?t=___REPLACE_IN_GULP___'
-      }
-      return config;
+      	if (config.url.indexOf('templates') !== -1) {
+        	config.url = config.url + '?t=___REPLACE_IN_GULP___'
+      	}
+      	return config;
     };
-  }]);
+}]);
 
-  app.config(['$httpProvider',function ($httpProvider) {
+app.config(['$httpProvider',function ($httpProvider) {
     $httpProvider.interceptors.push('preventTemplateCache');
-  }]);
+}]);
