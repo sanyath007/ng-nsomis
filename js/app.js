174a2d8f870d94a078ee1be5d7c0207a77b5d6e6
sanyath007 <sanyath007@gmail.com>
2020-12-01 04:12:22 +0700

    Add .htaccess file in root dir and edit app router

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
 var app = angular.module('App', ['ngRoute']);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true).hashPrefix('!');
 
 	$routeProvider
-
 	.when('/', {
 	    templateUrl: 'templates/home.html',
 	    controller: 'homeController'
 	})
-
 	.when('/page1', {
 	    templateUrl: 'templates/page1.html',
 	    controller: 'page1Controller'
 	})
-
 	.when('/page2', {
 	    templateUrl: 'templates/page2.html',
 	    controller: 'page2Controller'
+	})
+	.otherwise({
+		redirectTo: '/'
 	});
-}]);
+});
 
 app.controller('homeController', ['$scope', function($scope) {
 	$scope.message = 'Message on home page';
