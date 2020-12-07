a282a1f1e516d4d992eac3390adaa5374c2792f0
sanyath007 <sanyath007@gmail.com>
2020-12-07 10:21:44 +0700

    Edit bundle file in index.html and edit method name of er and or data ing homeController

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -191,6 +191,161 @@ app.controller('homeController', function($scope, $http, CONFIG, ReportService) 
         });
     }
 
+    $scope.getErVisit = function(e) {
+        if(e) e.preventDefault();
+        
+        let year = $scope.cboYear !== '' ? parseInt($scope.cboYear) - 543 : $scope.toDay.getFullYear();
+        console.log(year);
+        
+        ReportService.getSeriesData('er/visit/', year)
+        .then(function(res) {
+            let emergencyData = [];
+            let ugencyData = [];
+            let semiData = [];
+            let nonData = [];
+            let resusData = [];
+
+            res.data.visit.forEach((value, key) => {
+                let emergency = value.emergency ? parseInt(value.emergency) : 0;
+                let ugency = value.ugency ? parseInt(value.ugency) : 0;
+                let semi = value.semi ? parseInt(value.semi) : 0;
+                let non = value.non ? parseInt(value.non) : 0;
+                let resuscitation = value.resuscitation ? parseInt(value.resuscitation) : 0;
+
+                emergencyData.push(emergency);
+                ugencyData.push(ugency);
+                semiData.push(semi);
+                nonData.push(non);
+                resusData.push(resuscitation);
+
+            });
+
+            let series = [{
+                name: 'Emergency',
+                data: emergencyData,
+                color: '#e41749',
+            }, {
+                name: 'Ugency',
+                data: ugencyData,
+                color: '#f29c2b',
+            }, {
+                name: 'Semi-ugency',
+                data: semiData,
+                color: '#57D1C9',
+            }, {
+                name: 'Non-ugency',
+                data: nonData,
+                color: '#8bc24c',
+            }, {
+                name: 'Resuscitation',
+                data: resusData,
+                color: '#200A3E',
+            }];
+
+            var categories = ['ตค', 'พย', 'ธค', 'มค', 'กพ', 'มีค', 'เมย', 'พค', 'มิย', 'กค', 'สค', 'กย']
+            $scope.barOptions = ReportService.initBarChart("erVisitBarContainer", "ยอดผู้รับบริการรายเดือน ปีงบ " + (year + 543), categories, 'จำนวน');
+            $scope.barOptions.series = series;
+
+            var chart = new Highcharts.Chart($scope.barOptions);
+        }, function(err) {
+            console.log(err);
+        });
+    };
+
+    $scope.getErEmergency = function (e) {
+        if(e) e.preventDefault();
+        
+        let year = $scope.cboYear !== '' ? parseInt($scope.cboYear) - 543 : $scope.toDay.getFullYear();
+        console.log(year);
+        
+        ReportService.getSeriesData('/er/emergency/', year)
+        .then(function(res) {
+            var dataSeries = [];
+
+            $scope.pieOptions = ReportService.initPieChart("erEmergencyPieContainer", "สัดส่วนการบริการ ตามประเภทความเร่งด่วน", "", "สัดส่วนตามประเภทความเร่งด่วน");
+
+            res.data.emergency.forEach((value, key) => {
+                Object.keys(value).forEach(name => {
+                    $scope.pieOptions.series[0].data.push({name: name, y: parseInt(value[name])});
+                });
+            });
+
+            var chart = new Highcharts.Chart($scope.pieOptions);
+        }, function(err) {
+            console.log(err);
+        });
+    };
+    
+    $scope.getOrVisit = function(e) {
+        if(e) e.preventDefault();
+        
+        let year = $scope.cboYear !== '' ? parseInt($scope.cboYear) - 543 : $scope.toDay.getFullYear();
+        console.log(year);
+        
+        ReportService.getSeriesData('or/visit/', year)
+        .then(function(res) {
+            let smallData = [];
+            let largeData = [];
+            let otherData = [];
+
+            res.data.visit.forEach((value, key) => {
+                let small = value.small ? parseInt(value.small) : 0;
+                let large = value.large ? parseInt(value.large) : 0;
+                let other = value.other ? parseInt(value.other) : 0;
+
+                smallData.push(small);
+                largeData.push(large);
+                otherData.push(other);
+            });
+
+            let series = [{
+                name: 'ผ่าตัดเล็ก',
+                data: smallData,
+                color: '#e41749',
+            }, {
+                name: 'ผ่าตัดใหญ่',
+                data: largeData,
+                color: '#f29c2b',
+            }, {
+                name: 'อื่นๆ',
+                data: otherData,
+                color: '#57D1C9',
+            }];
+
+            var categories = ['ตค', 'พย', 'ธค', 'มค', 'กพ', 'มีค', 'เมย', 'พค', 'มิย', 'กค', 'สค', 'กย']
+            $scope.barOptions = ReportService.initBarChart("orVisitBarContainer", "ยอดผู้รับบริการรายเดือน ปีงบ " + (year + 543), categories, 'จำนวน');
+            $scope.barOptions.series = series;
+
+            var chart = new Highcharts.Chart($scope.barOptions);
+        }, function(err) {
+            console.log(err);
+        });
+    };
+
+    $scope.getOrType = function (e) {
+        if(e) e.preventDefault();
+        
+        let year = $scope.cboYear !== '' ? parseInt($scope.cboYear) - 543 : $scope.toDay.getFullYear();
+        console.log(year);
+        
+        ReportService.getSeriesData('/or/or-type/', year)
+        .then(function(res) {
+            var dataSeries = [];
+
+            $scope.pieOptions = ReportService.initPieChart("orTypePieContainer", "สัดส่วนผู้รับบริการผ่าตัด ตามประเภทการผ่าตัด", "", "สัดส่วนตามประเภทการผ่าตัด");
+
+            res.data.ortype.forEach((value, key) => {
+                Object.keys(value).forEach(name => {
+                    $scope.pieOptions.series[0].data.push({name: name, y: parseInt(value[name])});
+                });
+            });
+
+            var chart = new Highcharts.Chart($scope.pieOptions);
+        }, function(err) {
+            console.log(err);
+        });
+    };
+    
     $scope.getSumYearData = function (e) {
         if(e) e.preventDefault();
         
@@ -274,161 +429,6 @@ app.controller('homeController', function($scope, $http, CONFIG, ReportService) 
             });
 
             var chart = new Highcharts.Chart($scope.barOptions);
-        }, function(err) {
-            console.log(err);
-        });
-    };
-
-    $scope.getErVisitData = function(e) {
-        if(e) e.preventDefault();
-        
-        let year = $scope.cboYear !== '' ? parseInt($scope.cboYear) - 543 : $scope.toDay.getFullYear();
-        console.log(year);
-        
-        ReportService.getSeriesData('er/visit/', year)
-        .then(function(res) {
-            let emergencyData = [];
-            let ugencyData = [];
-            let semiData = [];
-            let nonData = [];
-            let resusData = [];
-
-            res.data.visit.forEach((value, key) => {
-                let emergency = value.emergency ? parseInt(value.emergency) : 0;
-                let ugency = value.ugency ? parseInt(value.ugency) : 0;
-                let semi = value.semi ? parseInt(value.semi) : 0;
-                let non = value.non ? parseInt(value.non) : 0;
-                let resuscitation = value.resuscitation ? parseInt(value.resuscitation) : 0;
-
-                emergencyData.push(emergency);
-                ugencyData.push(ugency);
-                semiData.push(semi);
-                nonData.push(non);
-                resusData.push(resuscitation);
-
-            });
-
-            let series = [{
-                name: 'Emergency',
-                data: emergencyData,
-                color: '#e41749',
-            }, {
-                name: 'Ugency',
-                data: ugencyData,
-                color: '#f29c2b',
-            }, {
-                name: 'Semi-ugency',
-                data: semiData,
-                color: '#57D1C9',
-            }, {
-                name: 'Non-ugency',
-                data: nonData,
-                color: '#8bc24c',
-            }, {
-                name: 'Resuscitation',
-                data: resusData,
-                color: '#200A3E',
-            }];
-
-            var categories = ['ตค', 'พย', 'ธค', 'มค', 'กพ', 'มีค', 'เมย', 'พค', 'มิย', 'กค', 'สค', 'กย']
-            $scope.barOptions = ReportService.initBarChart("erVisitBarContainer", "ยอดผู้รับบริการรายเดือน ปีงบ " + (year + 543), categories, 'จำนวน');
-            $scope.barOptions.series = series;
-
-            var chart = new Highcharts.Chart($scope.barOptions);
-        }, function(err) {
-            console.log(err);
-        });
-    };
-
-    $scope.getErEmergencyData = function (e) {
-        if(e) e.preventDefault();
-        
-        let year = $scope.cboYear !== '' ? parseInt($scope.cboYear) - 543 : $scope.toDay.getFullYear();
-        console.log(year);
-        
-        ReportService.getSeriesData('/er/emergency/', year)
-        .then(function(res) {
-            var dataSeries = [];
-
-            $scope.pieOptions = ReportService.initPieChart("erEmergencyPieContainer", "สัดส่วนการบริการ ตามประเภทความเร่งด่วน", "", "สัดส่วนตามประเภทความเร่งด่วน");
-
-            res.data.emergency.forEach((value, key) => {
-                Object.keys(value).forEach(name => {
-                    $scope.pieOptions.series[0].data.push({name: name, y: parseInt(value[name])});
-                });
-            });
-
-            var chart = new Highcharts.Chart($scope.pieOptions);
-        }, function(err) {
-            console.log(err);
-        });
-    };
-    
-    $scope.getOrVisitData = function(e) {
-        if(e) e.preventDefault();
-        
-        let year = $scope.cboYear !== '' ? parseInt($scope.cboYear) - 543 : $scope.toDay.getFullYear();
-        console.log(year);
-        
-        ReportService.getSeriesData('or/visit/', year)
-        .then(function(res) {
-            let smallData = [];
-            let largeData = [];
-            let otherData = [];
-
-            res.data.visit.forEach((value, key) => {
-                let small = value.small ? parseInt(value.small) : 0;
-                let large = value.large ? parseInt(value.large) : 0;
-                let other = value.other ? parseInt(value.other) : 0;
-
-                smallData.push(small);
-                largeData.push(large);
-                otherData.push(other);
-            });
-
-            let series = [{
-                name: 'ผ่าตัดเล็ก',
-                data: smallData,
-                color: '#e41749',
-            }, {
-                name: 'ผ่าตัดใหญ่',
-                data: largeData,
-                color: '#f29c2b',
-            }, {
-                name: 'อื่นๆ',
-                data: otherData,
-                color: '#57D1C9',
-            }];
-
-            var categories = ['ตค', 'พย', 'ธค', 'มค', 'กพ', 'มีค', 'เมย', 'พค', 'มิย', 'กค', 'สค', 'กย']
-            $scope.barOptions = ReportService.initBarChart("orVisitBarContainer", "ยอดผู้รับบริการรายเดือน ปีงบ " + (year + 543), categories, 'จำนวน');
-            $scope.barOptions.series = series;
-
-            var chart = new Highcharts.Chart($scope.barOptions);
-        }, function(err) {
-            console.log(err);
-        });
-    };
-
-    $scope.getOrTypeData = function (e) {
-        if(e) e.preventDefault();
-        
-        let year = $scope.cboYear !== '' ? parseInt($scope.cboYear) - 543 : $scope.toDay.getFullYear();
-        console.log(year);
-        
-        ReportService.getSeriesData('/or/or-type/', year)
-        .then(function(res) {
-            var dataSeries = [];
-
-            $scope.pieOptions = ReportService.initPieChart("orTypePieContainer", "สัดส่วนผู้รับบริการผ่าตัด ตามประเภทการผ่าตัด", "", "สัดส่วนตามประเภทการผ่าตัด");
-
-            res.data.ortype.forEach((value, key) => {
-                Object.keys(value).forEach(name => {
-                    $scope.pieOptions.series[0].data.push({name: name, y: parseInt(value[name])});
-                });
-            });
-
-            var chart = new Highcharts.Chart($scope.pieOptions);
         }, function(err) {
             console.log(err);
         });
