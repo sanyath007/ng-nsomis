62eb7f18d8ab153d39fd45d79caa0bd395d426f8
sanyath007 <sanyath007@gmail.com>
2021-01-13 16:07:45 +0700

    Complete pharma module both ip and op data

--- app/controllers/pharmaCtrl.js
+++ app/controllers/pharmaCtrl.js
@@ -3,32 +3,45 @@ app.controller('pharmaController', [
     '$scope',
     '$http',
     'CONFIG',
+    'StringFormatService',
     'toaster',
-    function($rootScope, $scope, $http, CONFIG, toaster)
+    function($rootScope, $scope, $http, CONFIG, StringFormatService, toaster)
     {
-        $scope.getIp = function(e) {
-            $http.get(`${CONFIG.apiUrl}/pharma/ip/${startDate}/${endDate}`)
-			.then(res => {
-				$scope.data = res.data;
+        $scope.data = [];
 
-				$scope.data.forEach((val, key) => {
-					$scope.totalData.qty += parseInt(val.sum_qty);
-					$scope.totalData.price += parseInt(val.sum_total);
-				});
+        $scope.getOp = function(e) {
+            if(e) e.preventDefault();
+
+            let startDate = ($('#sdate').val() !== '') 
+							? StringFormatService.convToDbDate($scope.sdate) 
+							: moment().format('YYYY-MM-DD');
+			let endDate = ($('#edate').val() !== '') 
+							? StringFormatService.convToDbDate($scope.edate) 
+                            : moment().format('YYYY-MM-DD');
+                            
+            $http.get(`${CONFIG.apiUrl}/pharma/op/${startDate}/${endDate}`)
+			.then(res => {
+                console.log(res);
+				$scope.data = res.data;
 			}, err => {
 				console.log(err)
 			});
         }
         
-        $scope.getOp = function(e) {
-            $http.get(`${CONFIG.apiUrl}/pharma/op/${startDate}/${endDate}`)
-			.then(res => {
-				$scope.data = res.data;
+        $scope.getIp = function(e) {
+            if(e) e.preventDefault();
 
-				$scope.data.forEach((val, key) => {
-					$scope.totalData.qty += parseInt(val.sum_qty);
-					$scope.totalData.price += parseInt(val.sum_total);
-				});
+            let startDate = ($('#sdate').val() !== '') 
+							? StringFormatService.convToDbDate($scope.sdate) 
+							: moment().format('YYYY-MM-DD');
+			let endDate = ($('#edate').val() !== '') 
+							? StringFormatService.convToDbDate($scope.edate) 
+                            : moment().format('YYYY-MM-DD');
+                            
+            $http.get(`${CONFIG.apiUrl}/pharma/ip/${startDate}/${endDate}`)
+			.then(res => {
+                console.log(res);
+				$scope.data = res.data;
 			}, err => {
 				console.log(err)
 			});
