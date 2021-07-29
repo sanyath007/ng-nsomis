4e47690f4cdc57942d6796f6fe95dead93831724
sanyath007 <sanyath007@gmail.com>
2021-07-29 11:00:24 +0700

    Modify getNumTambon of covidCtrl and re-design covid/num-tambon

--- app/controllers/covidCtrl.js
+++ app/controllers/covidCtrl.js
@@ -8,10 +8,18 @@ app.controller('covidController', [
 	{
 		$scope.cboDate = '';
 		$scope.data = [];
+		$scope.totalData = {};
 
+		const initTotalData = function() {
+			return {
+				total: 0,
+			}
+		};
 		$scope.getNumTambon = function(e) {
 			if(e) e.preventDefault();
-			
+
+			$scope.totalData = initTotalData();
+
 			$scope.loading = true;
             let date = ($scope.cboDate !== '') 
                         ? StringFormatService.convToDbDate($scope.cboDate)
@@ -19,25 +27,11 @@ app.controller('covidController', [
 
 			$http.get(`${CONFIG.apiUrl}/covid/num-tambon/${date}`)
 			.then(res => {
-				console.log(res);
-				// $scope.data = res.data.vision;
+				$scope.data = res.data;
 
-				// $scope.data && $scope.data.forEach(async (visit) => {
-				// 	let resDc = await $http.get(`${CONFIG.apiUrl}/eye/visio2020/followup/${visit.hn}/${visit.vn}/1`);					
-				// 	let dc = (resDc.data.length > 0) ? resDc.data[0] : null;
-					
-				// 	if (dc) {
-				// 		let resFu1 = await $http.get(`${CONFIG.apiUrl}/eye/visio2020/followup/${visit.hn}/${dc.vn}/0`);					
-				// 		let fu1 = (resFu1.data.length > 0) ? resFu1.data[0] : null;
-				// 		visit.fu1 = fu1;
-				// 	}
-
-				// 	if (visit.fu1) {
-				// 		let resFu2 = await $http.get(`${CONFIG.apiUrl}/eye/visio2020/followup/${visit.hn}/${visit.fu1.vn}/0`);					
-				// 		let fu2 = (resFu2.data.length > 0) ? resFu2.data[0] : null;
-				// 		visit.fu2 = fu2;
-				// 	}
-				// })
+				$scope.totalData.total = $scope.data.reduce((sum, tam) => {
+					return sum + parseInt(tam.num_pt);
+				}, 0);
 			}, err => {
 				console.log(err);
 			});
